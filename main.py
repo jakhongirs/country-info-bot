f1911ff9bbf2698554350261204bb7324855d6d2
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-07 13:41:25 +0500

    added logic

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext
+from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext, MessageHandler
 from telegram.update import Update
+from telegram.ext.filters import Filters
+import requests
 import settings
 
 updater = Updater(settings.TELEGRAM_TOKEN)
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Salom!")
+    update.message.reply_text(
+        "Assalomu Alaykum, davlatlar haqida maʼlumot oluvchi botga xush kelibsiz! Misol: /country Uzbekistan")
+
+
+def country(update: Update, context: CallbackContext):
+    args = context.args
+
+    if len(args) == 0:
+        update.message.reply_text("Notug'ri formatdagi so'rov! Misol: /country Uzbekistan")
+    else:
+        country_text = ' '.join(args)
+
+        response = requests.get(f'https://restcountries.com/v3.1/name/{country_text}')
+
+        result = response.json()
+
+        update.message.reply_text(result[0]['name']['common'])
 
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('country', country))
+dispatcher.add_handler(MessageHandler(Filters.all, start))
 
 updater.start_polling()
 updater.idle()
